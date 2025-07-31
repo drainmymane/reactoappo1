3a41f890fe1ef9b8ae83b280c0fbac38cf3fa759
drainmymane <gordermpf1@gmail.com>
2025-07-31 16:56:11 +0300

    'aas'

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import './styles/App.css'
 import ItemsList from "./components/ItemsList";
 import ItemsForm from "./components/ItemsForm";
@@ -7,6 +7,10 @@ import AwesomeButton from "./components/UI/button/AwesomeButton";
 import ItemsFilter from "./components/ItemsFilter";
 import { useItems } from "./hooks/useItems";
 import axios from 'axios';
+//import ItemsService from "../API/ItemsService";
+import generateRandomItems from "../API/api";
+import { motion, AnimatePresence } from "framer-motion";
+import { UseFetch } from "./hooks/useFetch";
 
 
 
@@ -17,15 +21,22 @@ function App() {
     if (a < b) return -1;
   }
 
-  let [items, setItems] = useState([
+  const [items, setItems] = useState([
     { id: 1, title: 'Apple', amount: 20, },
     { id: 2, title: 'bi', amount: -1 },
     { id: 3, title: 'ZOD', amount:5 },
   ])
 
-  let [modalVisbility, setModalVisibility]=useState(false);
-  let [filter,setFilter]=useState({sort:'',query:''})
+  const [modalVisbility, setModalVisibility]=useState(false)
+  const [filter,setFilter]=useState({sort:'',query:''})
+  const [fetchItems,isItemsLoading, ItemsError] = UseFetch(async ()=>{
+      setItems(generateRandomItems(100)); //const items = await ItemsService.getAll();
+  })
   let sortedAndSearchedItems = useItems(items, filter.sort, filter.query)
+
+  useEffect(()=>{
+    fetchItems()
+  },[])
 
   const createItem = (newItem) => {
     setItems([...items, newItem]);
@@ -37,6 +48,7 @@ function App() {
 
   return (
     <div className="App">
+      <AwesomeButton onClick={()=>{console.log(items)}}></AwesomeButton>
       <AwesomeButton onClick={()=>setModalVisibility(true)}>
         Add New Item!!!
       </AwesomeButton>
@@ -44,7 +56,16 @@ function App() {
         <ItemsForm create={createItem}></ItemsForm>
       </MyModal>
       <ItemsFilter filter={filter} setFilter={setFilter} />
-      <ItemsList remove={removeItem} items={sortedAndSearchedItems} title={'My ANime List'}></ItemsList>
+      {
+        ItemsError &&
+        <h1>we have an exscuse: ${ItemsError}</h1>
+      }
+      {
+        isItemsLoading 
+          ? <motion.div className="rotatingSqr" animate={{ rotate: 380 }} transition={{ duration: 1, ease: "easeInOut", repeat: Infinity,repeatType: "reverse" }}>Loading</motion.div>
+          : <ItemsList remove={removeItem} items={sortedAndSearchedItems} title={'My ANime List'}></ItemsList>
+      }
+
     </div>
   );
 }
